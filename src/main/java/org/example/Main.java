12d375d4da3ead125aeb185fb701b7c8008a6323
md-ashikul-islam <ashikul005@gmail.com>
2023-02-06 21:55:36 +0600

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.setProperty("webdriver.edge.driver","C:\\Users\\Ashik\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+
+        driver.get(("http://www.google.com"));
+        driver.quit();
+    }
+}
